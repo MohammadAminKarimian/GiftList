2063e7a09471ecb2e4191fc2d7858df317423922
Mohammad amin Karimian <alpha1amk@gmail.com>
2025-02-05 15:47:16 +0330

    taskDone

--- server/index.js
+++ server/index.js
@@ -1,29 +1,32 @@
-const express = require('express');
-const verifyProof = require('../utils/verifyProof');
+const express = require("express");
+const verifyProof = require("../utils/verifyProof");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
 
-const port = 1225;
+const port = 3000;
 
 const app = express();
 app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT =
+  "ddd59a2ffccddd60ff47993312821cd57cf30f7f14fb82937ebe2c4dc78375aa";
 
-app.post('/gift', (req, res) => {
+app.post("/gift", (req, res) => {
   // grab the parameters from the front-end here
-  const body = req.body;
+  const name = req.body.name;
+  const proof = req.body.proof;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
+  // TODO: prove that a name is in the list
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+  if (isInTheList) {
     res.send("You got a toy robot!");
-  }
-  else {
+  } else {
     res.send("You are not on the list :(");
   }
 });
 
-app.listen(port, () => {
+app.listen(port, "127.0.0.1", () => {
   console.log(`Listening on port ${port}!`);
 });
--- utils/example.js
+++ utils/example.js
@@ -1,6 +1,6 @@
-const MerkleTree = require('./MerkleTree');
-const niceList = require('./niceList');
-const verifyProof = require('./verifyProof');
+const MerkleTree = require("./MerkleTree");
+const niceList = require("./niceList");
+const verifyProof = require("./verifyProof");
 
 // create the merkle tree for the whole nice list
 const merkleTree = new MerkleTree(niceList);
@@ -8,12 +8,19 @@ const merkleTree = new MerkleTree(niceList);
 // get the root
 const root = merkleTree.getRoot();
 
-// find the proof that norman block is in the list 
-const name = 'Norman Block';
-const index = niceList.findIndex(n => n === name);
+// find the proof that norman block is in the list
+const name = "Norman Block";
+const index = niceList.findIndex((n) => n === name);
 const proof = merkleTree.getProof(index);
 
+console.log(`tree root: ${root}`);
+
 // verify proof against the Merkle Root
-console.log( verifyProof(proof, name, root) ); // true, Norman Block is in the list!
+console.log(verifyProof(proof, name, root)); // true, Norman Block is in the list!
 
 // TRY IT OUT: what happens if you try a name not in the list, or a fake proof?
+const fakeName = "Amin Block";
+console.log(verifyProof(proof, fakeName, root));
+
+proof[2].left = !proof[2].left;
+console.log(verifyProof(proof, name, root));
